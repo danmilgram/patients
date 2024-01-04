ac2de3791938aa39a0ecaa4266b08fd44bc343c4
Dan Milgram <ddmilgram@gmail.com>
2024-01-04 16:07:10 -0300

    add tests

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -57,6 +57,31 @@ def test_post_patient():
     assert response.status_code == 200
     assert response.json() == payload
 
+def test_post_patient_invalid_content_type():
+
+    file_path = '{}/tests/test-image.jpg'.format(os.getcwd())
+    response = client.post(
+        '/patients',
+        files={'document': ('document.jpg', open(file_path, 'rb'), 'image/gif')},
+        data=payload,
+    )
+    assert response.status_code == 415
+    assert response.json() == {'detail': 'Unsupported Media Type. Only image files (JPEG, PNG) are allowed.'}
+
+def test_post_patient_invalid_email():
+
+    invalid_email_payload = payload.copy()
+    invalid_email_payload.update({'email':'thisisaninvalidemail'})
+
+    file_path = '{}/tests/test-image.jpg'.format(os.getcwd())
+    response = client.post(
+        '/patients',
+        files={'document': ('document.jpg', open(file_path, 'rb'), 'image/jpeg')},
+        data=invalid_email_payload,
+    )
+    assert response.status_code == 400
+    assert response.json()['detail'][0]['msg'] == "value is not a valid email address: The email address is not valid. It must have exactly one @-sign."
+
 def test_post_existing_patient():
 
     file_path = '{}/tests/test-image.jpg'.format(os.getcwd())
